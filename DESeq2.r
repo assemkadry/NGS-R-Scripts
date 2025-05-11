92a5bdc5f749c28646d66a4129d529bda4cc2f9f
Assem K. Elsherif <135189593+assemkadry@users.noreply.github.com>
2025-05-11 15:11:47 +0300

    DESeq2.r

--- DESeq2.r
+++ DESeq2.r
@@ -1,11 +1,11 @@
 #!/usr/bin/env Rscript
 
-# Run with: cat counts.txt | Rscript deseq2_pipe.R 3x3
+# Usage: cat counts.txt | Rscript deseq2_clean.R 3x3
 
 args <- commandArgs(trailingOnly = TRUE)
 
 if (length(args) != 1) {
-  stop("Usage: Rscript deseq2_pipe.R NxM  (e.g., 3x3)", call. = FALSE)
+  stop("Usage: Rscript deseq2_clean.R NxM (e.g., 3x3)", call. = FALSE)
 }
 
 # Parse design
@@ -14,43 +14,39 @@ cond1_num <- as.integer(design[1])
 cond2_num <- as.integer(design[2])
 conditions <- factor(c(rep("cond1", cond1_num), rep("cond2", cond2_num)))
 
-# Load libraries
-suppressMessages({
-  library(DESeq2)
-})
+suppressMessages(library(DESeq2))
 
-# Read from stdin
+# Read count data
 count_data <- read.table(file("stdin"), header = TRUE, row.names = 1, sep = "\t")
-
-# Ensure integer counts
 count_matrix <- round(as.matrix(count_data))
 mode(count_matrix) <- "integer"
 
-# Create metadata
+# Metadata
 col_data <- data.frame(condition = conditions)
 rownames(col_data) <- colnames(count_matrix)
 
-# Create DESeq2 object
+# DESeq2
 dds <- DESeqDataSetFromMatrix(countData = count_matrix,
                               colData = col_data,
                               design = ~ condition)
-
-dds <- dds[rowSums(counts(dds)) > 10, ]  # filter low counts
+dds <- dds[rowSums(counts(dds)) > 10, ]
 dds <- DESeq(dds)
 
-# Differential expression
+# Results
 res <- results(dds, contrast = c("condition", "cond1", "cond2"))
 res <- res[order(res$padj), ]
-write.table(res, file = "", sep = "\t", quote = FALSE)
 
-# Get DEGs with padj < 0.05
-de_ids <- rownames(subset(res, padj < 0.05))
+# Format numbers safely
+res_df <- as.data.frame(res)
+res_df <- format(res_df, digits = 6, scientific = TRUE)
+res_df <- cbind(id = rownames(res_df), res_df)
 
-# Normalized counts
+# Save results safely
+write.table(res_df, file = "results_deseq2.tsv", sep = "\t", quote = FALSE, row.names = FALSE)
+
+# Save normalized counts of DE genes
+de_ids <- rownames(subset(res, padj < 0.05 & !is.na(padj)))
 norm_counts <- counts(dds, normalized = TRUE)
 norm_df <- data.frame(id = rownames(norm_counts), norm_counts)
 de_df <- norm_df[norm_df$id %in% de_ids, ]
-
-# Save
-write.table(de_df, file = "norm-matrix-deseq2.txt", sep = "\t",
-            quote = FALSE, row.names = FALSE, col.names = TRUE)
+write.table(de_df, file = "norm-matrix-deseq2.tsv", sep = "\t", quote = FALSE, row.names = FALSE)
